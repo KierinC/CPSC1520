589567984ee3d1570c0f6abcfd0a3928792ee47c
Kierin Wanless <kierin.chesworth@gmail.com>
2022-09-28 17:13:35 -0600

    in class 3 start

---
+++ In Class/in-class-3-start/js/main.js
@@ -0,0 +1,12 @@
+// step 1: READ THE PDF (step 2 and 3 is READ THE PDF)
+
+// ensure your js is linked.
+
+// select the javascript list.
+// to clarify not a single item but the entire list.
+
+// add a mouse over link to the javascript list to highlight an item.
+
+// add an event listener on mouse out that remove the active class
+
+// add an event listener that will open links in new tab.
