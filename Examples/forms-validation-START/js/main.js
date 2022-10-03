e639525709a93c2918fcb8cf1fd82888f4532c71
KierinC <Kierin.chesworth@gmail.com>
2022-10-03 16:40:55 -0600

    forms validation start

---
+++ Examples/forms-validation-START/js/main.js
@@ -0,0 +1,31 @@
+// we're going to use bootstrap validation
+// https://getbootstrap.com/docs/5.0/forms/validation/
+/*
+  essentially what we're going to do here.
+  - check if each form element's value is valid.
+  - essentially we're going to pass the values to validator functions
+      - these functions are going to return true or false values
+        true if valid, false 
+  - if it's invalid we're going to add the class "is-invalid"
+      - this is going to show the divs with the "invalid-feedback"
+        which has the 
+  - also we're going to return early from the event handler if it's not valid
+      - this is so we don't create the budget items.
+*/
+
+// intercept the form.
+
+// create a variable to hold the running budget total
+
+// focus to the first element on page load
+
+// add a submit listener
+
+// function to add a budget item create function
+// e.g.
+// <li class="list-group-item list-group-item-action" aria-current="true">
+// TITLE AMOUNT - DESCRIPTION</li>
+
+// function to add a updateTotal function
+
+// function for general currency formatter
