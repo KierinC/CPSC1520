fe0d865e50e6c7068d1c21d09f8abeb3d6875f8f
KierinC <Kierin.chesworth@gmail.com>
2022-10-05 12:27:42 -0600

    forms validator

--- Examples/forms-validation-START/js/main.js
+++ Examples/forms-validation-START/js/main.js
@@ -13,19 +13,60 @@
       - this is so we don't create the budget items.
 */
 
-// intercept the form.
+(function (){
 
-// create a variable to hold the running budget total
+  // intercept the form and required elements
+  let budgetFormElement = document.querySelector('#budget-form');
+  let budgetList = document.querySelector('.current-budget');
+  let budgetTotalElement = document.querySelector('#budget-total');
 
-// focus to the first element on page load
+  //TODO: validate the values
+  
 
-// add a submit listener
+  // create a variable to hold the running budget total
+  let runningBudgetTotal = 0;
 
-// function to add a budget item create function
+  // focus to the first element on page load
+  budgetFormElement.elements['budget-title'].focus();
+
+  // add a submit listener
+  budgetFormElement.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    let budgetItemTitle = evt.target.elements['budget-title'].value;
+    let itemAmount = parseFloat(evt.target.elements['amount'].value);
+    let itemDescription = evt.target.elements['budget-description'].value;
+
+    // Add item to list
+    AddBudgetItem(budgetItemTitle, itemAmount, itemDescription);
+
+    // Clear form
+    budgetFormElement.reset();
+
+    // Focus on first element again
+    budgetFormElement.elements['budget-title'].focus();
+
+    // Update budget total
+    UpdateTotal(itemAmount);
+  });
+
+// function to add a budget item
 // e.g.
 // <li class="list-group-item list-group-item-action" aria-current="true">
 // TITLE AMOUNT - DESCRIPTION</li>
+function AddBudgetItem(name, amount, description){
+  budgetList.innerHTML += `<li class="list-group-item list-group-item-action"> ${name} ${FormatMoney(amount)} - ${description}</li>`;
+}
 
 // function to add a updateTotal function
+function UpdateTotal(amount) {
+  runningBudgetTotal += amount;
+  budgetTotalElement.innerText = FormatMoney(runningBudgetTotal);
+}
 
 // function for general currency formatter
+function FormatMoney(number) {
+  return new Intl.NumberFormat('en-CA', { style: 'currency', currency: 'CAD' }).format(number);
+}
+
+})();
