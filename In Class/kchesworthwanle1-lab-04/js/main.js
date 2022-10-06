89d4f7e15999fce2455f4434448532acabb45a87
KierinC <Kierin.chesworth@gmail.com>
2022-10-06 09:43:46 -0600

    lab 4 complete

--- In Class/kchesworthwanle1-lab-04/js/main.js
+++ In Class/kchesworthwanle1-lab-04/js/main.js
@@ -1,32 +1,75 @@
-let newUsersList = document.querySelector(".new-users-list")
+(function (){
+    let newUsersList = document.querySelector(".new-users-list");
+    let formElement = document.querySelector(".new-user-form");
 
-// READ THE PDF BEFORE STARTING
-// Step 1-5 is read the pdf.
+    // READ THE PDF BEFORE STARTING
+    // Step 1-5 is read the pdf.
 
-// the isValueNotEmpty function: 
-// returns true if value not empty
-// returns false if the value is empty
-const isValueNotEmpty = (value) => {
-    if (value !== "") {
-        return true
+    formElement.addEventListener("submit", (evt) => {
+        evt.preventDefault();
+
+        let usernameElement = evt.target.elements["username"];
+        let cityElement = evt.target.elements["city"];
+        let provinceElement = evt.target.elements["province"];
+
+        let usernameValue = usernameElement.value;
+        let cityValue = cityElement.value;
+        let provinceValue = provinceElement.value;
+
+        let isFormValid = true;
+
+        if (isValueNotEmpty(usernameValue) && !hasWhiteSpace(usernameValue)) {
+            usernameElement.classList.remove("is-invalid");
+        } else {
+            usernameElement.classList.add("is-invalid");
+            isFormValid = false;
+        }
+
+        if (isValueNotEmpty(cityValue)) {
+            cityElement.classList.remove("is-invalid");
+        } else {
+            cityElement.classList.add("is-invalid");
+            isFormValid = false;
+        }
+
+        if (isValueNotEmpty(provinceValue)) {
+            provinceElement.classList.remove("is-invalid");
+        } else {
+            provinceElement.classList.add("is-invalid");
+            isFormValid = false;
+        }
+        
+        if (isFormValid) {
+            //console.log(usernameValue, cityValue, provinceValue);
+            addUser(usernameValue, cityValue, provinceValue);
+        }
+    });
+
+    // the isValueNotEmpty function: 
+    // returns true if value not empty
+    // returns false if the value is empty
+    const isValueNotEmpty = (value) => {
+        if (value !== "") {
+            return true
+        }
+        return false
     }
-    return false
-}
 
-// the hasWhiteSpace function
-// returns true if it has whitespace
-// returns false if it does not has whitespace
-function hasWhiteSpace(s) {
-    return (/\s/).test(s);
-}
+    // the hasWhiteSpace function
+    // returns true if it has whitespace
+    // returns false if it does not has whitespace
+    function hasWhiteSpace(s) {
+        return (/\s/).test(s);
+    }
 
-// adds user to the page.
-const addUser = (username, city, province) => {
-    let newUser = `<li class="list-group-item d-flex justify-content-between align-items-start">
-    <div class="ms-2 me-auto">
-        <div class="fw-bold">${username}</div>
-        ${city}, ${province}
-    </div>
-    </li>`
-    newUsersList.innerHTML = newUsersList.innerHTML + newUser
-}
+    // adds user to the page.
+    const addUser = (username, city, province) => {
+        let newUser = `<li class="list-group-item d-flex justify-content-between    align-items-start">
+        <div class="ms-2 me-auto">
+            <div class="fw-bold">${username}</div>
+            ${city}, ${province}
+        </div>
+        </li>`
+        newUsersList.innerHTML = newUsersList.innerHTML + newUser;
+    }
+})();
