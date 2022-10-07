47683eea030fd600f37bc0fbdc416a0a8512781e
KierinC <Kierin.chesworth@gmail.com>
2022-10-07 08:29:42 -0600

    Assignment upload

---
+++ Assignments/assignment-1-start/js/main.js
@@ -0,0 +1,19 @@
+// wrap everything in an IIFE
+const USER = 'Your name';
+
+// Please refer to the "Required Tasks in the assignments PDF"
+
+// html for the add cheep create function
+/*
+    <li class="text-center list-group-item list-group-item-action" aria-current="true">
+        <p>Author: USERNAME HERE</p>
+        <div class="row h-100">
+            <div class="col-sm">
+                <img src="CHEEP GIF HERE" class="rounded float-start" alt="CHEEP GIF HERE">    
+            </div>
+            <div class="col-sm h-100">
+                <h5 class="mb-1">CHEEP TEXT HERE</5>
+            </div>
+        </div>
+    </li>
+*/
