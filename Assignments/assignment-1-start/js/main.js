3e5e57a77d6c784a38b3a2e6fcc1a834e73ef506
KierinC <Kierin.chesworth@gmail.com>
2022-10-12 12:55:20 -0600

    progress

--- Assignments/assignment-1-start/js/main.js
+++ Assignments/assignment-1-start/js/main.js
@@ -1,10 +1,11 @@
 // wrap everything in an IIFE
-
 (function () {
     const USER = 'Your name';
-    let formElement = document.querySelector('.cheep-form');
+    let formElement = document.querySelector('#cheep-form');
+    let listElement = document.querySelector('.cheep-list');
 
-    console.log('.js file linked');
+    // focus on first element on page load
+    formElement.elements['cheep-text'].focus();
 
     formElement.addEventListener('submit', (evt) => {
         evt.preventDefault();
@@ -12,18 +13,69 @@
         let cheeptextElement = evt.target.elements['cheep-text'];
         let cheepgifElement = evt.target.elements['cheep-gif'];
 
-        
+        let cheeptextValue = evt.target.elements['cheep-text'].value;
+        let cheepgifValue = evt.target.elements['cheep-gif'].value;
+
+         // form validation
+        let isFormValid = true;
+
+        if (!isTextNotEmpty(cheeptextValue)) {
+          isFormValid = false;
+
+          cheeptextElement.classList.add("is-invalid");
+        } else {
+          cheeptextElement.classList.remove("is-invalid");
+        }
+
+        if (isFormValid) {
+            // Add item to list
+            AddCheep(cheeptextValue, cheepgifValue);
+
+            // Clear form
+            formElement.reset();
+
+            // Focus on first element again
+            formElement.elements['cheep-text'].focus();
+        }
     });
 
     // function to determine if field is not empty
     function isTextNotEmpty (value) {
         let valid = false;
         
-        if (value.length > 0) {
+        if (value.length > 0 && value.length < 50) {
           valid = true;
         }
     
         return valid;
+    }
+
+    // function to determine if a gif is selected
+    function isGifSelected (value) {
+        let valid = false;
+
+        if (value.lenth > 0) {
+            valid = true;
+        }
+
+        return valid;
+    }
+
+    // function to add a cheep
+    function AddCheep(text, gif) {
+        let oldContent = listElement.innerHTML;
+
+        listElement = `<li class="text-center list-group-item list-group-item-action" aria-current="true">
+        <p>Author: ${USER}</p>
+        <div class="row h-100">
+            <div class="col-sm">
+                <img src="${gif}" class="rounded float-start" alt="CHEEP GIF HERE">    
+            </div>
+            <div class="col-sm h-100">
+                <h5 class="mb-1">${text}</5>
+            </div>
+        </div>
+    </li>` + oldContent;
     }
 })();
 
