9f7fe8643cd30f36d2fe55fe1a10c77f81ad4eb8
KierinC <Kierin.chesworth@gmail.com>
2022-10-13 09:43:54 -0600

    loops movies demo complete

--- Examples/loops-movies-example-START/js/main.js
+++ Examples/loops-movies-example-START/js/main.js
@@ -1,21 +1,45 @@
-// this is our javascript file
+(function () {
+    // intercept the form and/or required elements
+    let movieFilterForm = document.querySelector('#movie-filter-form');
 
-// intercept the form and/or required elements
+    // focus on the filter element
+    movieFilterForm.elements['filter-query'].focus();
 
-// focus on the filter element
+    // select all movie list items the descendant css selector
+    // .top-movies-list li
+    let allMovieItems = document.querySelectorAll('.top-movies-list li');
 
-// select all movie list items the descendant css selector
-// .top-movies-list li
+    movieFilterForm.addEventListener('submit', (evt) => {
+        evt.preventDefault();
 
-// get the form value and call the function filterItems
+        // get the form value and call the function filterItems
+        let filter = evt.target.elements['filter-query'].value;
+        //console.log(filter);
+        filterItems(filter);
+    });
 
-/*
+    /*
+    We're going to create a function named "filterItems"
+    that check each item contains our search query
+    - if it's not, then we're going to add the "hidden-item" class
+    - if does include it, we're going to remove the "hidden-item" class
 
-We're going to create a function named "filterItems"
-that check each item contains our search query
-- if it's not, then we're going to add the "hidden-item" class
-- if does include it, we're going to remove the "hidden-item" class
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/   String/includes
+    */
 
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+    function filterItems(filterValue) {
+        let lowCaseFilterValue = filterValue.toLowerCase();
+        
+        for (let i = 0; i < allMovieItems.length; i++) {
+            let lowCaseMovieTitle = allMovieItems[i].innerText.toLowerCase();
 
-*/
+            if (lowCaseMovieTitle.includes(lowCaseFilterValue)) {
+                allMovieItems[i].classList.remove('hidden-item');
+            } else {
+                allMovieItems[i].classList.add('hidden-item');
+            }
+        }
+    }
+
+
+})();
