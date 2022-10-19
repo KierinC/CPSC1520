b0ec5f6a375c13b5646ccfc2eece17d951be24a3
KierinC <Kierin.chesworth@gmail.com>
2022-10-19 10:13:50 -0600

    object fundamentals start

---
+++ Examples/todo-object-fundamentals-START/js/main.js
@@ -0,0 +1,32 @@
+
+/*
+    Todolist
+
+    We're going to make a todolist that counts how many todos we've completed.
+    We'll do this using objects in the todos array shown below.
+
+    HTML FOR The todo
+
+    <li class="list-group-item">
+        <input class="form-check-input todo-status"
+            todo-id="INDEX HERE"
+            type="checkbox"
+            value="TODO VALUE WITH INDEX HERE" aria-label="..."
+            TODO COMPLETE TERNARY HERE>
+        DESCRIPTION HERE
+
+    </li>
+
+*/
+
+let todos = [
+    {
+        description: 'Todo 1',
+        complete: false,
+
+    },
+    {
+        description: 'Todo 2',
+        complete: true,
+    }
+];
--- In Class/kchesworthwanle1-lab-05/js/main.js
+++ In Class/kchesworthwanle1-lab-05/js/main.js
@@ -12,7 +12,21 @@ Steps 1-3 READ THE PDF!!!
   videoGameForm.addEventListener('submit', (event) => {
     event.preventDefault();
     let platform = event.target.elements['platform-family'].value.toLowerCase();
+
+    filterGames(platform);
   });
+
+  function filterGames(platform) {
+    allGameListItems.forEach((el, i) => {
+      let gamePlatform = allGameListItems[i].innerText.toLowerCase();
+
+      if (gamePlatform.includes(platform)) {
+        allGameListItems[i].classList.remove('hidden-item');
+      } else {
+        allGameListItems[i].classList.add('hidden-item');
+      }
+    });
+  }
 
   // event listener for step 2
   /* HTML for step 2 to add to the list
@@ -20,10 +34,37 @@ Steps 1-3 READ THE PDF!!!
   */
   allGameList.addEventListener('click', (event) => {
     let game = event.target.innerText;
+
+    addToFavouriteGames(game);
   });
+
+  function addToFavouriteGames(game) {
+    myGames.push(game);
+    console.log(myGames);
+    renderFavouriteList();
+  }
+
+  function renderFavouriteList() {
+    myGameList.innerHTML = '';
+
+    myGames.forEach((el) => {
+      myGameList.innerHTML += `<li class="list-group-item">${el}</li>`
+    });
+  }
 
   // event listener for step 3
   myGameList.addEventListener('click', (event) => {
     let favGame = event.target.innerText;
+
+    removeFromGameList(favGame);
   });
+
+  function removeFromGameList(game) {
+    let index = myGames.indexOf(game);
+
+    myGames.splice(index, 1);
+
+    renderFavouriteList();
+  }
+
 })();
