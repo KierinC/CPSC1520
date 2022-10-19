8e235547fbcbab38bd503071763acd56108771f5
KierinC <Kierin.chesworth@gmail.com>
2022-10-19 12:15:57 -0600

    object fundametals complete

--- Examples/todo-object-fundamentals-START/js/main.js
+++ Examples/todo-object-fundamentals-START/js/main.js
@@ -1,4 +1,3 @@
-
 /*
     Todolist
 
@@ -11,7 +10,7 @@
         <input class="form-check-input todo-status"
             todo-id="INDEX HERE"
             type="checkbox"
-            value="TODO VALUE WITH INDEX HERE" aria-label="..."
+            value="TODO VALUE WITH INDEX HERE"
             TODO COMPLETE TERNARY HERE>
         DESCRIPTION HERE
 
@@ -19,14 +18,86 @@
 
 */
 
-let todos = [
-    {
-        description: 'Todo 1',
-        complete: false,
+(function () {
+    let todos = [
+        {
+            description: 'Todo 1',
+            complete: false,
+    
+        },
+        {
+            description: 'Todo 2',
+            complete: true,
+        }
+    ];
+    
+    let todoListElement = document.querySelector('.todo-list');
+    let todoFormElement = document.querySelector('#add-todo-form');
+    let updateCountElement = document.querySelector('#todo-complete-count');
 
-    },
-    {
-        description: 'Todo 2',
-        complete: true,
+    todoListElement.addEventListener('change', (evt) => {
+        let todoCheck = evt.target;
+        let todoId = todoCheck.getAttribute('todo-id');
+
+        // toggle the complete property for the todo
+        todos[todoId].complete = !todos[todoId].complete;
+        // TODO: update the count
+        updateCount();
+    });
+
+    todoFormElement.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+
+        let todoDescription = evt.target.elements['todo-description'].value;
+        addTodo(todoDescription);
+    });
+
+    function updateCount() {
+        let count = 0;
+
+        // intro to reduce() function
+        let finalCount = todos.reduce((previousValue, todo) => {
+            if (todo.complete) {
+                previousValue += 1;
+            }
+            return previousValue;
+        }, count);
+
+        updateCountElement.innerText = finalCount;
+
+        // more simple for loop method
+        // for (todo of todos) {
+        //     if (todo.complete) {
+        //         count += 1;
+        //     }
+        // }
+        // updateCountElement.innerText = count;
     }
-];
+    
+    function addTodo(todoDescription) {
+        let newTodo = {
+            description: todoDescription,
+            complete: false,
+        };
+        todos.push(newTodo);
+        renderTodos();
+    }
+
+    function renderTodos() {
+        todoListElement.innerHTML = '';
+    
+        todos.forEach((todo, idx) => {
+            todoListElement.innerHTML += `<li class="list-group-item">
+                <input class="form-check-input todo-status"
+                todo-id="${idx}"
+                type="checkbox"
+                value="todo-${idx}"
+                ${todo.complete? 'checked' : ''}>
+                ${todo.description}
+            </li>`;
+        });
+    }
+    
+    renderTodos();
+    updateCount();
+})();
