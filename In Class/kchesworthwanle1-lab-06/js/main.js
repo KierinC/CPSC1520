d2c9d5daf9ca1014c88f3ed68c47b3cdb49b3f54
KierinC <Kierin.chesworth@gmail.com>
2022-10-27 09:40:59 -0600

    lab 6 complete

--- In Class/kchesworthwanle1-lab-06/js/main.js
+++ In Class/kchesworthwanle1-lab-06/js/main.js
@@ -5,6 +5,30 @@ HTML for the project.
   <div class="d-flex w-100 justify-content-between">
     <h5 class="mb-1">FULLNAME HERE</h5>
   </div>
- <small>EMAIL HERE<</small>
+ <small>EMAIL HERE</small>
 </div>
 */
+(function () {
+  let allContacts = document.querySelector('.all-contacts');
+
+  getContacts(renderContacts);
+
+  function renderContacts(contactsData) {
+    allContacts.innerHTML = '';
+
+    contactsData.forEach((contact) => {
+      allContacts.innerHTML += `<div class="list-group-item list-group-item-action">
+      <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">${contact.name}</h5>
+      </div>
+      <small>${contact.email}</small>
+      </div>`;
+    });
+  }
+  
+  function getContacts(callback) {
+    fetch('data/contacts.json')
+    .then((res) => res.json())
+    .then((contacts) => callback(contacts));
+  }
+})();
