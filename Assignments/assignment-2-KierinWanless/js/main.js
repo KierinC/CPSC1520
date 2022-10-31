28f21b26e11e8119222201a299188a5eca2e5e0a
KierinC <Kierin.chesworth@gmail.com>
2022-10-31 15:07:18 -0600

    assignment 2 upload

---
+++ Assignments/assignment-2-KierinWanless/js/main.js
@@ -0,0 +1,14 @@
+/*
+HTML for table rows.
+<tr>
+    <td>TITLE HERE</td>
+    <td>AUTHOR HERE</td>
+    <td>DATE HERE</td>
+    <td><a href="LINK HERE">view talk</a></td>
+    <td>VIEWS HERE</td>
+    <td>LIKES HERE</td>
+</tr>
+
+*/
+
+let allTedTalks = [];
